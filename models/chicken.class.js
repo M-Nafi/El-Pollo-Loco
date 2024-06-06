a4dec67b588327bd41fe8c44d5cb3f7886f4f48f
M-Nafi <nafi-m@gmx.de>
2024-06-06 21:50:52 +0200

    added random startposition from chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,6 +2,8 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.x = 300 + Math.random() * 500;  // starten von links 250 px bis max 500 dazwischen zufällige zahl
+        // bei 720px breite. später entsprechend anpassen !!!
     }
 
 }
