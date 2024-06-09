3e1d499a05d478fa309b9e5b0d383377922611b0
M-Nafi <nafi-m@gmx.de>
2024-06-09 15:01:05 +0200

    added first background-object. changing in chicken heights

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,8 +2,8 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        // this.height = 80;  // hiermit wird die höhe der chickens festfelegt
-        // this.y = 330;     // hiermit wird horizontale posiiton festgelegt, wo die chickens laufen
+        this.height = 80;  // hiermit wird die höhe der chickens festfelegt
+        this.y = 330;     // hiermit wird horizontale posiiton festgelegt, wo die chickens laufen
         this.x = 300 + Math.random() * 500;  // starten von links 300 px bis max 500 dazwischen zufällige zahl
         // bei 720px breite. später entsprechend anpassen !!!
     }
