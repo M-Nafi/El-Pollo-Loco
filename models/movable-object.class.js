eeeb532f6467b35e18e330e7b79e9d5b36a92f04
M-Nafi <nafi-m@gmx.de>
2024-06-09 22:12:58 +0200

    added character walking animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,23 @@ class MovableObject {
     img;
     height = 280;
     width = 100;
+    imageCache = [];
 
     loadImage(path) {
         this.img = new Image(); // ist das gleiche wie document.getElementById('image')...
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = img;    
+        });
     }
 
     moveRight() {
