cb5268fc242edc7e8ce5dce13ba059f9b1161f5d
M-Nafi <nafi-m@gmx.de>
2024-06-09 13:58:19 +0200

    added first clouds in world and adjust chicken width, y and x position

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,7 +2,9 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.x = 300 + Math.random() * 500;  // starten von links 250 px bis max 500 dazwischen zufällige zahl
+        // this.height = 80;  // hiermit wird die höhe der chickens festfelegt
+        // this.y = 330;     // hiermit wird horizontale posiiton festgelegt, wo die chickens laufen
+        this.x = 300 + Math.random() * 500;  // starten von links 300 px bis max 500 dazwischen zufällige zahl
         // bei 720px breite. später entsprechend anpassen !!!
     }
 
---
+++ models/cloud.class.js
@@ -0,0 +1,10 @@
+class Cloud extends MovableObject {
+  y = 20;
+  height = 200;
+  width = 700;
+
+  constructor() {
+    super().loadImage("img/5_background/layers/4_clouds/1.png");
+    this.x = Math.random() * 500;
+  }
+}
