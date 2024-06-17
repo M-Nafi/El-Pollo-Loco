0dcb835a667308d90fccdc05fd595a1fd432da3d
M-Nafi <nafi-m@gmx.de>
2024-06-17 22:29:16 +0200

    added jump funktion for main character and adjusting sone code for clean coding

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,46 +1,65 @@
 class MovableObject {
-    x = 120;
-    y = 145;
-    img;
-    height = 280;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15;
-    otherDirection = false;
+  x = 120;
+  y = 145;
+  img;
+  height = 280;
+  width = 100;
+  imageCache = {};
+  currentImage = 0;
+  speed = 0.15;
+  otherDirection = false;
+  speedY = 0;
+  acceleration = 2; // geschwindigkeit fallen. anpassen!
 
-    loadImage(path) {
-        this.img = new Image(); // ist das gleiche wie document.getElementById('image')...
-        this.img.src = path;
-    }
-
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = img;    
-        });
-    }
-
-    playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length; // i = ist aufgrund der modulo (& bzw. ist der rest...) die reihenfolge von images walking in der unendlich schleife
-        // praktisch sieht das so aus: i = 0, 1, 2, 3, 4, 5 und dann nicht 6 sondern wieder von vorne 0, 1, 2 usw...
-        let path = images[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-    }
-
-    moveRight() {
-        console.log('Moving right');
-    }
-
-    moveLeft() {
-        setInterval(() => {
-          this.x -= this.speed;   // statt this speed kann man auch 0.15 reinschreiben. 
-        }, 1000 / 60);
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
       }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 140;
+  }
+
+  loadImage(path) {
+    this.img = new Image(); // ist das gleiche wie document.getElementById('image')...
+    this.img.src = path;
+  }
+
+  /**
+   *
+   * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  playAnimation(images) {
+    let i = this.currentImage % this.IMAGES_WALKING.length; // i = ist aufgrund der modulo (& bzw. ist der rest...) die reihenfolge von images walking in der unendlich schleife
+    // praktisch sieht das so aus: i = 0, 1, 2, 3, 4, 5 und dann nicht 6 sondern wieder von vorne 0, 1, 2 usw...
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
+
+  moveRight() {
+    this.x += this.speed;
+   
+  }
+
+  moveLeft() {
+    this.x -= this.speed; // statt this speed kann man auch 0.15 reinschreiben.
+  
+  }
+
+  jump() {
+    this.speedY = 20; // höhe des sprung evtl anpassen
+  }
 }
--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,29 +1,29 @@
 class smallChicken extends MovableObject {
-    y = 375;     // hiermit wird horizontale posiiton festgelegt, wo die chickens laufen
-    height = 40;  // hiermit wird die höhe der chickens festfelegt
-    width = 45;
-    IMAGES_WALKING = [
-        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
-        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
-        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
-    ];
+  y = 375; // hiermit wird horizontale posiiton festgelegt, wo die chickens laufen
+  height = 40; // hiermit wird die höhe der chickens festfelegt
+  width = 45;
+  IMAGES_WALKING = [
+    'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+  ];
 
-    constructor() {
-        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');  
-        this.loadImages(this.IMAGES_WALKING);      
-        this.x = 500 + Math.random() * 1700;  // starten von links 300 px bis max 500 dazwischen zufällige zahl
-        // bei 720px breite. später entsprechend anpassen !!!
-        this.speed = 0.17 + Math.random() * 0.45;
-        this.animate();
-    }
+  constructor() {
+    super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+    this.loadImages(this.IMAGES_WALKING);
+    this.x = 500 + Math.random() * 1700; // starten von links 300 px bis max 500 dazwischen zufällige zahl
+    // bei 720px breite. später entsprechend anpassen !!!
+    this.speed = 0.17 + Math.random() * 0.45;
+    this.animate();
+  }
 
-    animate() {
-        this.moveLeft();
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
 
-        setInterval(() => {
-          this.playAnimation(this.IMAGES_WALKING);
-        }, 150);  // geschwindigkeit der chicken später entsprechend anpassen
-       
-      }
-
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 150); // geschwindigkeit der chicken später entsprechend anpassen
+  }
 }
