53bfe598e56287c281bda1568827e76a946ce89f
M-Nafi <nafi-m@gmx.de>
2024-06-20 10:12:39 +0200

    added bottle rotation imgs if character throws bottles

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,7 +1,15 @@
 class ThrowableObject extends MovableObject {
 
+    BOTTLE_ROTATION = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.BOTTLE_ROTATION);
         this.x = x;
         this.y = y;
         this.height = 60;
@@ -11,9 +19,11 @@ class ThrowableObject extends MovableObject {
 
     throw() {       
         this.speedY = 20;
+        
         this.applyGravity();
         setInterval(() => {
-            this.x += 7;            
+            this.x += 7;
+            this.playAnimation(this.BOTTLE_ROTATION);                        
         }, 25);
     }
 }
